8471827fa9fb44440886532994d5d70e7201d8ca
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-03-27 01:28:21 +0330

    ✨ show profile url added

--- bluebank/user/urls.py
+++ bluebank/user/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from user.views import sign_up
+from user.views import sign_up, show_profile
 
 urlpatterns = [
     path('sign-up', sign_up),
+    path('show-profile/<int:user_id>', show_profile)
 ]
