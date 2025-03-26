6d8135f4e6b343ea40840786cbc433c8e990c893
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-03-26 18:41:04 +0330

    ✨ views for actions added

--- bluebank/actions/views.py
+++ bluebank/actions/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from actions.models import Card
+import json
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+def deposite(request, card_id : int):
+    data = json.loads(request.body)
+    desired_card = get_object_or_404(Card, card_id)
+    desired_card.amount += data.get("amount")
+    desired_card.save()
+
+def wire_money(request, sender_id : int, reciever_id : int):
+    sender = get_object_or_404(Card, id = sender_id)
+    reciever = get_object_or_404(Card, id = reciever_id)
+
+    data = json.loads(request.body)
+
+    if sender.amount < data.get("amount"):
+
+        sender.amount -= data.get("amount")
+        reciever.amount += data.get("amount")
+
+        sender.save()
+        reciever.save()
