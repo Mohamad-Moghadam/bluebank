3279c84b45cd40ddd95bdfccc1cee61cb017a7e5
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-03-27 03:03:17 +0330

    ✨ show balance url added

--- bluebank/actions/urls.py
+++ bluebank/actions/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from actions.views import deposite, wire_money
+from actions.views import deposite, wire_money, show_balance
 
 urlpatterns = [
     path('deposite/<int:card_id>', deposite),
     path('wire-money/<int:sender_id>/<int:reciever_id>', wire_money),
+    path('show_balance', show_balance)
 ]
