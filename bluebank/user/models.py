0137c45e4505fa45e4edb9a35ffbbb4d9d71a634
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-03-26 17:57:01 +0330

    🐛 default added

--- bluebank/user/models.py
+++ bluebank/user/models.py
@@ -5,4 +5,4 @@ class User(models.Model):
     name = models.CharField(max_length = 100)
     last_name = models.CharField(max_length = 100)
     user_name = models.CharField(max_length = 100)
-    cards = models.ForeignKey(to = Card, on_delete = models.PROTECT, related_name = "users_cards")
+    cards = models.ForeignKey(to = Card, on_delete = models.PROTECT, related_name = "users_cards", default = 1)
