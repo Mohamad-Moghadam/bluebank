584edc5e3e758af02c2f54f2db3cf0acf0dc0251
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-03-26 17:57:32 +0330

    ✨ sign-up feature added

--- bluebank/user/views.py
+++ bluebank/user/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
+from user.models import User
+import json
 
 @csrf_exempt
 def sign_up(request):
-    data = 
+    data = json.loads(request.body)
+    
+    User.objects.create(
+        name = data.get("name"),
+        last_name = data.get("last_name"),
+        user_name = data.get("user_name"),
+        cards = data.get("cards"),
+    )
 
