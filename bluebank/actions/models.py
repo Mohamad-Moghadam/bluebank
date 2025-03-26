36581180d92622f057a90b52911eef66cad23892
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-03-26 18:08:36 +0330

    🐛 colour choices fixed

--- bluebank/actions/models.py
+++ bluebank/actions/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
 class Card(models.Model):
-    CARDS = ["green", "purple", "yellow", "white", "red", "blue"]
+    CARDS = [("green", "Green"), ("purple", "Purple"), ("yellow", "Yellow"), ("white", "White"), ("red", "Red"), ("blue", "Blue")]
     colour = models.CharField(max_length = 100, choices = CARDS, null = True, blank = True, default = "blue")
     amount = models.IntegerField(default = 0, editable = False)
