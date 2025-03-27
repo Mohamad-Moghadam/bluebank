f97f7ccb9fbc1c3f153f79a4ad2156be51e41755
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-03-27 13:50:12 +0330

    ✨ loan model added

--- bluebank/actions/models.py
+++ bluebank/actions/models.py
@@ -4,5 +4,11 @@ from user.models import User
 class Card(models.Model):
     CARDS = [("green", "Green"), ("purple", "Purple"), ("yellow", "Yellow"), ("white", "White"), ("red", "Red"), ("blue", "Blue")]
     colour = models.CharField(max_length = 100, choices = CARDS, null = True, blank = True, default = "blue")
-    amount = models.IntegerField(default = 0, editable = False)
+    amount = models.BigIntegerField(default = 0, editable = False)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="cards_of_the_user")
+
+
+class Loan(models.Model):
+    card = models.ForeignKey(Card, on_delete= models.CASCADE, related_name= "loans_of_each_card")
+    amount = models.BigIntegerField()
+    number_of_installation = models.IntegerField()
