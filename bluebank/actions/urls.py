73f7e3bbfa6912b0f9d912479cf1ddf8d72cc676
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-03-27 14:28:11 +0330

    ✨ paying installation urls added

--- bluebank/actions/urls.py
+++ bluebank/actions/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from actions.views import deposite, wire_money, show_balance, get_loan
+from actions.views import deposite, wire_money, show_balance, get_loan, pay_installation
 
 urlpatterns = [
     path('deposite/<int:card_id>', deposite),
     path('wire-money/<int:sender_id>/<int:reciever_id>', wire_money),
     path('show_balance/<int:card_id>', show_balance),
-    path('get-loan/<int:card_id>', get_loan)
+    path('get-loan/<int:card_id>', get_loan),
+    path('pay-installation/<int:loan_id>/<int:card_id>', pay_installation)
 ]
