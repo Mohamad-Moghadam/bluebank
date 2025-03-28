01819caefe53e3552ee9f852c7f0d8fe38271c7a
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-03-28 16:32:45 +0330

    🎉 poject completed

--- bluebank/actions/models.py
+++ bluebank/actions/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from user.models import User
+from phonenumber_field.modelfields import PhoneNumberField
 
 class Card(models.Model):
     CARDS = [("green", "Green"), ("purple", "Purple"), ("yellow", "Yellow"), ("white", "White"), ("red", "Red"), ("blue", "Blue")]
     colour = models.CharField(max_length = 100, choices = CARDS, null = True, blank = True, default = "blue")
     amount = models.BigIntegerField(default = 0, editable = False)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="cards_of_the_user")
+    number = PhoneNumberField(region = "IR")
 
 
 class Loan(models.Model):
